cc537c373cf8360af06ee95fcaedd1075d4a69a7
YourMan <george@socialab.gr>
2020-10-25 22:56:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,16 +17,16 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup( 
-    name = 'linkedin_scraper', 
-    packages = ['linkedin_scraper'], # this must be the same as the name above 
+    name = 'linkedin_scraper_touchup', 
+    packages = ['linkedin_scraper_touchup'], # this must be the same as the name above 
     version = version, 
     description = 'Scrapes user data from Linkedin', 
     long_description = long_description,
     long_description_content_type='text/markdown',
-    author = 'Joey Sham', 
-    author_email = 'sham.joey@gmail.com', 
-    url = 'https://github.com/joeyism/linkedin_scraper', # use the URL to the github repo 
-    download_url = 'https://github.com/joeyism/linkedin_scraper/dist/' + version + '.tar.gz', 
+    author = 'George A.', 
+    author_email = 'ganagnos@gmail.com', 
+    url = 'https://github.com/YourMan/linkedin_scraper_touchup', # use the URL to the github repo 
+    download_url = 'https://github.com/YourMan/linkedin_scraper_touchup/dist/' + version + '.tar.gz', 
     keywords = ['linkedin', 'scraping', 'scraper'],
     classifiers = [], 
     install_requires=[package.split("\n")[0] for package in open("requirements.txt", "r").readlines()]
